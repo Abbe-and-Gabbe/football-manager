9d9c74bf2deb5d3f4e82c4f7ebb26d8c5147c9bd
Albin Forsberg <foal21fs@student.ju.se>
2023-02-02 15:02:51 +0100

    Cosmetic changes

--- football-manager/src/lib/Navbar.svelte
+++ football-manager/src/lib/Navbar.svelte
@@ -6,8 +6,8 @@
 <header
   class="w-auto h-12 flex justify-between items-center bg-slate-200 dark:bg-slate-900 text-black dark:text-white p-10"
 >
-  <div class="items-center">
-    <h1 class="text-2xl font-bold">Football Manager</h1>
+  <div class="items-center w-1/2">
+    <h1 class="text-2xl font-bold prose-h1">Football Manager</h1>
   </div>
   <UserNav/>
 </header>
--- football-manager/src/lib/UserNav.svelte
+++ football-manager/src/lib/UserNav.svelte
@@ -2,10 +2,20 @@
   let loggedIn = true;
 </script>
 
-<div class="text-black dark:text-white p-8 flex justify-between">
+<div class="text-black dark:text-white p-8 flex">
   {#if loggedIn}
-      <img src="images/userImg.jpg" alt="userImg" class="w-10 h-10 rounded-full m-2" />
-      <img src="images/bell.png" alt="userImg" class="w-10 h-10 rounded-full m-2 dark:invert" />
+    <div class="flex">
+      <img
+        src="images/userImg.jpg"
+        alt="userImg"
+        class="w-7 h-7 rounded-full m-2"
+      />
+      <img
+        src="images/bell.png"
+        alt="userImg"
+        class="w-7 h-7 rounded-full dark:invert m-2"
+      />
+    </div>
   {:else}
     <button
       class="rounded bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 "
