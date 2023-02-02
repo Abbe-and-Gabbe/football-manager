8fa7af0bc9f167dc68df7d124b0400595000f560
Albin Forsberg <foal21fs@student.ju.se>
2023-02-02 15:47:54 +0100

    Added little content

--- football-manager/src/App.svelte
+++ football-manager/src/App.svelte
@@ -6,7 +6,15 @@
 </script>
 
 <Navbar />
-<main class="dark:bg-slate-700 bg-slate-100 dark:text-white m-2 rounded-xl p-12 h-auto">
-  hELLO"
+<main class="dark:bg-slate-800 bg-slate-100 dark:text-white m-2 rounded-xl p-12 h-auto shadow-xl">
+  <!-- Fill this with content -->
+  <section class="prose dark:prose-invert my-2 bg-slate-600 rounded-xl p-2 w-full">
+    <h1>
+      Welcome to Football manager
+    </h1>
+  </section>
+  <section class="prose dark:prose-invert">
+    <Counter />
+  </section>
 </main>
 <Footer/>
