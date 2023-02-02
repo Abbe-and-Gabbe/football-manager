69b01df9dc98180c1bcfa54dc909f5d75a54c386
Albin Forsberg <foal21fs@student.ju.se>
2023-02-02 22:08:42 +0100

    Only shows notify when there are to show.

--- football-manager/src/lib/UserNav.svelte
+++ football-manager/src/lib/UserNav.svelte
@@ -36,12 +36,14 @@
         >
         <span class="sr-only">Notifications</span>
         Notifications
+        {#if count > 0}        
         <div
           class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900"
           class:animate-bounce={count > 0}
         >
           {count}
         </div>
+        {/if}
       </button>
     </div>
   {:else}
