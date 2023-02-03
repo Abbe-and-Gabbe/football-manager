2cd6609103f71c0c9c3d43215bd53ce0fc443f20
Albin Forsberg <foal21fs@student.ju.se>
2023-02-03 18:44:59 +0100

    Color cordination dor dark mode on home

--- football-manager/src/lib/Components/Card.svelte
+++ football-manager/src/lib/Components/Card.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div
-  class="prose dark:prose-invert bg-slate-300 dark:bg-slate-700 flex-row w-full text-center p-12 border rounded-xl hover:dark:bg-slate-600 hover:bg-slate-400"
+  class="prose dark:prose-invert bg-slate-300 dark:bg-slate-700 flex-row w-full text-center p-12 border border-slate-600 rounded-xl hover:dark:bg-slate-600 hover:bg-slate-400 hover:dark:border-blue-500"
 >
   <h3>
     {cardTitle}
--- football-manager/src/lib/Pages/Home.svelte
+++ football-manager/src/lib/Pages/Home.svelte
@@ -4,12 +4,12 @@
 
 <!-- This is far from styled correctly, don't even bother to comment this... -->
 
-<section class="animate-expand-in flex-col">
+<section class="animate-fade-in flex-col">
   <div
-    class="bg-slate-200 dark:bg-slate-600 flex justify-evenly flex-col p-12 lg:flex-row gap-2 rounded-xl items-center lg:mx-44 animate-fade-in"
+    class="bg-slate-200 dark:bg-slate-800 flex justify-evenly flex-col p-12 lg:flex-row gap-2 rounded-xl items-center lg:mx-44 animate-fade-in"
   >
     <div
-      class="prose dark:prose-invert p-12 border rounded-xl bg-slate-300 dark:bg-slate-700 flex-row w-full text-center"
+      class="prose dark:prose-invert p-12 border border-slate-600 rounded-xl bg-slate-300 dark:bg-slate-800 flex-row w-full text-center"
     >
       <h1>Welcome!</h1>
 
@@ -23,11 +23,26 @@
     class="h-px my-8 border border-slate-200 dark:border-slate-700 rounded-xl"
   />
 
-  <div class="w-full m-auto flex">
-    <div class="flex justify-evenly gap-2 m-auto">
-        <Card cardTitle="Team management" cardDescription="Manage your team" />
-        <Card cardTitle="Statistics" cardDescription="Gather all your statistics at one place, such as goals and assists" />
-        <Card cardTitle="Players" cardDescription="Follow the greatest upcoming players" />
+  <div class="bg-slate-400 dark:bg-slate-800 p-12 rounded-xl shadow-2xl">
+    <div class="prose dark:prose-invert w-full m-auto text-center mb-12">
+      <h2>Features</h2>
+    </div>
+
+    <div class="w-full m-auto flex">
+      <div class="grid lg:grid-flow-col auto-rows-max m-auto gap-2">
+        <Card
+          cardTitle="Team management"
+          cardDescription="Manage your team, keep track of your players and staff"
+        />
+        <Card
+          cardTitle="Statistics"
+          cardDescription="Gather all your statistics at one place, such as goals and assists"
+        />
+        <Card
+          cardTitle="Players"
+          cardDescription="Follow the greatest upcoming players"
+        />
+      </div>
     </div>
   </div>
 </section>
