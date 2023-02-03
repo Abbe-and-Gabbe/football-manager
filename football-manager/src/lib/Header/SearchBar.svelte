c19d2c5f812366e71af29864761c8a84474e09ba
Albin Forsberg <foal21fs@student.ju.se>
2023-02-03 10:19:35 +0100

    Toggle login with login button...

--- football-manager/src/lib/Header/SearchBar.svelte
+++ football-manager/src/lib/Header/SearchBar.svelte
@@ -53,11 +53,6 @@
         required
         on:input={search}
       />
-      <button
-        type="submit"
-        class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >Search</button
-      >
     </div>
   </form>
   <SearchResultsContainer {searchResults}/>
--- football-manager/src/lib/Header/UserNav.svelte
+++ football-manager/src/lib/Header/UserNav.svelte
@@ -18,6 +18,9 @@
   {:else}
     <button
       class="relative inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      on:click={() => {
+        loggedIn = true;
+      }}
     >
       Login
     </button>
