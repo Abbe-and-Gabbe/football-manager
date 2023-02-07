ab196b4241bdaee8d52e0701e84a94c5b05880e8
Albin Forsberg <foal21fs@student.ju.se>
2023-02-07 23:12:20 +0100

    Added team navigation

---
+++ football-manager/src/lib/Components/NavButton.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { link } from "svelte-routing";
+
+  export let id;
+  export let buttonText = "Click Me";
+  export let location = "/";
+</script>
+
+<a
+  href={location}
+  use:link
+  class="bg-slate-400 dark:bg-slate-500 p-3 rounded-xl dark:text-white hover:bg-slate-500 hover:dark:bg-slate-600"
+>
+  {buttonText}
+</a>
--- football-manager/src/lib/Pages/Team/Team.svelte
+++ football-manager/src/lib/Pages/Team/Team.svelte
@@ -1,8 +1,9 @@
 <script>
     export let id;
     import teams from "../../../assets/data";
+  import TeamNav from "./TeamNav.svelte";
     let team = teams.find((item) => item.id == id);
 
 </script>
 
-<h1>Hello {team.name}</h1>
+<TeamNav id="{id}" />
