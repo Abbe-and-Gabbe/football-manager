a006425b2251535675e6ca8818f071b912b535d8
Albin Forsberg <foal21fs@student.ju.se>
2023-02-07 22:53:41 +0100

    Added Team page

--- football-manager/src/App.svelte
+++ football-manager/src/App.svelte
@@ -5,6 +5,7 @@
   import Navbar from "./lib/Header/Navbar.svelte";
   import { Router, Link, Route } from "svelte-routing";
   import Home from "./lib/Pages/Home.svelte";
+  import Team from "./lib/Pages/Team/Team.svelte";
 
   export let url = "";
 </script>
@@ -15,6 +16,7 @@
     class="dark:bg-slate-900 bg-slate-100 dark:text-white p-12 shadow-xl h-full"
   >
   <Route path="/" component={Home} />
+  <Route path="/team/:id" component={Team} />
   </main>
 </Router>
 <Footer />
--- football-manager/src/assets/data.js
+++ football-manager/src/assets/data.js
@@ -1,54 +1,89 @@
 // Football team
 
-const teams = [{
+const teams = [
+  {
+    id: 1,
+    name: "Arsenal",
+    logo: "https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg",
+    players: [
+      {
         id: 1,
-        name: "Arsenal",
-        logo: "https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg",
-    },
-    {
+        name: "Pierre-Emerick Aubameyang",
+        position: "Forward",
+        number: 14,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/5/5e/Aubameyang_2019.jpg",
+      },
+      {
         id: 2,
-        name: "Aston Villa",
-        logo: "https://upload.wikimedia.org/wikipedia/en/9/9f/Aston_Villa.svg",
-    },
-    {
+        name: "Bernd Leno",
+        position: "Goalkeeper",
+        number: 19,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/9/9c/Bernd_Leno_2019.jpg",
+      },
+      {
         id: 3,
-        name: "Brighton & Hove Albion",
-        logo: "https://upload.wikimedia.org/wikipedia/en/f/fd/Brighton_%26_Hove_Albion_logo.svg",
-    },
-    {
+        name: "Emile Smith Rowe",
+        position: "Midfielder",
+        number: 10,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/9/9c/Emile_Smith-Rowe_2019.jpg",
+      },
+      {
         id: 4,
-        name: "Burnley",
-        logo: "https://upload.wikimedia.org/wikipedia/en/0/02/Burnley_FC_badge.svg",
-    },
-    {
-        id: 5,
-        name: "Chelsea",
-        logo: "https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg",
-    },
-    {
-        id: 6,
-        name: "Crystal Palace",
-        logo: "https://upload.wikimedia.org/wikipedia/en/4/4c/Crystal_Palace_F.C._logo.svg",
-    },
-    {
-        id: 7,
-        name: "Everton",
-        logo: "https://upload.wikimedia.org/wikipedia/en/f/f9/Everton_FC_logo.svg",
-    },
-    {
-        id: 8,
-        name: "Fulham",
-        logo: "https://upload.wikimedia.org/wikipedia/en/0/0b/Fulham_FC_%28shield%29.svg",
-    },
-    {
-        id: 9,
-        name: "Leeds United",
-        logo: "https://upload.wikimedia.org/wikipedia/en/1/11/Leeds_United_Logo.svg",
-    },
-    {
-        id: 10,
-        name: "Mullsjö IF",
-    },
+        name: "Gabriel Martinelli",
+        position: "Forward",
+        number: 27,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/9/9c/Gabriel_Martinelli_2019.jpg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Aston Villa",
+    logo: "https://upload.wikimedia.org/wikipedia/en/9/9f/Aston_Villa.svg",
+  },
+  {
+    id: 3,
+    name: "Brighton & Hove Albion",
+    logo: "https://upload.wikimedia.org/wikipedia/en/f/fd/Brighton_%26_Hove_Albion_logo.svg",
+  },
+  {
+    id: 4,
+    name: "Burnley",
+    logo: "https://upload.wikimedia.org/wikipedia/en/0/02/Burnley_FC_badge.svg",
+  },
+  {
+    id: 5,
+    name: "Chelsea",
+    logo: "https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg",
+  },
+  {
+    id: 6,
+    name: "Crystal Palace",
+    logo: "https://upload.wikimedia.org/wikipedia/en/4/4c/Crystal_Palace_F.C._logo.svg",
+  },
+  {
+    id: 7,
+    name: "Everton",
+    logo: "https://upload.wikimedia.org/wikipedia/en/f/f9/Everton_FC_logo.svg",
+  },
+  {
+    id: 8,
+    name: "Fulham",
+    logo: "https://upload.wikimedia.org/wikipedia/en/0/0b/Fulham_FC_%28shield%29.svg",
+  },
+  {
+    id: 9,
+    name: "Leeds United",
+    logo: "https://upload.wikimedia.org/wikipedia/en/1/11/Leeds_United_Logo.svg",
+  },
+  {
+    id: 10,
+    name: "Mullsjö IF",
+  },
 ];
 
 // Export teams
