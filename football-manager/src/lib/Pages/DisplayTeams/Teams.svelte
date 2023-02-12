0b1e3917ddb0b4e5ada1cbc84eca3cee43f4b6a9
Gabriel Adward <adga21bv@student.ju.se>
2023-02-12 14:44:55 +0100

    Added list of team for each section

--- football-manager/src/lib/Pages/DisplayTeams/Teams.svelte
+++ football-manager/src/lib/Pages/DisplayTeams/Teams.svelte
@@ -1,35 +1,10 @@
 <script>
   export let id;
   import DtNavbar from "./DTNavbar.svelte";
-  import teamsData from "../../../assets/TeamsData";
+  import DisplayAllTeams from "./DisplayAllTeams.svelte";
 
 </script>
 
 <DtNavbar {id} />
-<section class="animate-fade-in flex-col mt-10">
-  <div
-    class="bg-black dark:bg-slate-800 static justify-evenly flex-col p-12 lg:flex-row gap-2 rounded-xl items-center lg:mx-44 animate-fade-in"
-  >
-    <h1 class="text-left mb-10 text-3xl">Teams</h1>
-    {#each teamsData as team}
-      <div
-        class="w-full bg-zinc-800 dark:bg-zinc-800 p-4"
-      >
-        <h4
-          class="font-bold rounded-2xl border-8 border-black p-4 bg-slate-600	 flex justify-center"
-        >
-          {team.titleName}
-        </h4>
-        <div class="flex flex-col ml-10 mr-10">
-          <ul>
-            {#each team.teamNameForText as teamName}
-              <li class="border-2 border-black p-2 rounded-xl bg-slate-800">
-                <a href="/team/:id">{teamName}</a>
-              </li>
-            {/each}
-          </ul>
-        </div>
-      </div>
-    {/each}
-  </div>
-</section>
+<DisplayAllTeams/>
+
