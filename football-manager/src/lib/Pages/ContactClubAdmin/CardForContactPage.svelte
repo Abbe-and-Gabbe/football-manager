44be5b3ac96d25a6c6edb43e69b99eb6d9639749
Gabriel Adward <adga21bv@student.ju.se>
2023-02-22 09:40:25 +0100

    changed navbar

--- football-manager/src/lib/Pages/ContactClubAdmin/CardForContactPage.svelte
+++ football-manager/src/lib/Pages/ContactClubAdmin/CardForContactPage.svelte
@@ -6,7 +6,7 @@
     <div class="bg-slate-500 dark:bg-slate-800 static justify-evenly flex-col p-4 md:p-12 lg:flex-row gap-2 rounded-xl items-center lg:mx-44 animate-fade-in">
       <h1 class="text-left mb-10 text-3xl">Contact Page</h1>
   
-      <div class="min-h-fit bg-gray-100 leading-6 py-20">
+      <div class="min-h-fit bg-gray-100 leading-6 py-20 bg-slate-600	">
         <div class="grid grid-cols-1 md:grid-cols-3 gap-8 p-5 max-w-screen-lg mx-auto">
           {#each contactPersons as person}
           <div class="relative shadow-md rounded-md border-8 border-black text-center bg-gray-800 p-4 md:p-8">
--- football-manager/src/lib/Pages/ContactClubAdmin/ContactPageNavbar.svelte
+++ football-manager/src/lib/Pages/ContactClubAdmin/ContactPageNavbar.svelte
@@ -6,7 +6,7 @@
 <div class="bg-slate-400 dark:bg-slate-800 w-full rounded-xl p-2 gap-2 flex" style="overflow-x: auto;">
     <NavButton buttonText="Home" location="/club/{id}" {id} />
     <NavButton buttonText="Teams" location="/club/{id}/teams" {id} />
-    <NavButton buttonText="Calendar" location="/team/{id}/calendar" {id} />
+    <NavButton buttonText="Calendar" location="/club/{id}/calendar" {id} />
     <NavButton buttonText="Contact" location="/club/{id}/contact" {id} />
     <!-- Only visible to admins: -->
     <NavButton buttonText="Admin" location="/team/{id}/admin" {id} />
