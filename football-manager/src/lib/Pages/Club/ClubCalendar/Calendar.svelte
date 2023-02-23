f67408537366e8663f88bc16a45d7dc40e8c2e96
GabrielAdward <adga21bv@student.ju.se>
2023-02-23 20:54:00 +0100

    A calender displayed on screen

--- football-manager/src/lib/Pages/Club/ClubCalendar/Calendar.svelte
+++ football-manager/src/lib/Pages/Club/ClubCalendar/Calendar.svelte
@@ -1,13 +1,135 @@
 <script>
     import ClubNavBar from "../clubNavBar.svelte";
     export let id;
+    const events = [
+    { date: new Date(2023, 1, 14), title: 'Valentine\'s Day' },
+    { date: new Date(2023, 2, 17), title: 'St. Patrick\'s Day' },
+    { date: new Date(2023, 3, 1), title: 'April Fools\' Day' },
+    { date: new Date(2023, 4, 9), title: 'Mother\'s Day' },
+    { date: new Date(2023, 5, 19), title: 'Father\'s Day' },
+    { date: new Date(2023, 6, 4), title: 'Independence Day' },
+    { date: new Date(2023, 7, 7), title: 'Labor Day' },
+    { date: new Date(2023, 8, 11), title: 'Patriot Day' },
+    { date: new Date(2023, 9, 31), title: 'Halloween' },
+    { date: new Date(2023, 10, 23), title: 'Thanksgiving Day' },
+    { date: new Date(2023, 11, 25), title: 'Christmas Day' },
+    { date: new Date(2024, 0, 1), title: 'New Year\'s Day' }
+  ];
+
+  // Current date
+  let currentDate = new Date();
+
+  // Months and weekdays
+  const months = [
+    'January', 'February', 'March', 'April',
+    'May', 'June', 'July', 'August', 'September',
+    'October', 'November', 'December'
+  ];
+  const weekdays = [ 'Sun','Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+  // Calculate days in month
+  function daysInMonth(month, year) {
+    return new Date(year, month + 1, 0).getDate();
+  }
+
+  // Calculate week number
+  function getWeekNumber(date) {
+    const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+    const daysSinceFirstDayOfYear = (date - firstDayOfYear) / 86400000;
+    return Math.ceil((firstDayOfYear.getDay() + 1 + daysSinceFirstDayOfYear) / 7);
+  }
+
+  // Generate days in month
+  function generateDaysInMonth(month, year) {
+    const days = [];
+    const firstDayOfMonth = new Date(year, month, 1);
+    const lastDayOfMonth = new Date(year, month, daysInMonth(month, year));
+    const daysBeforeFirstDayOfMonth = firstDayOfMonth.getDay();
+
+    // Add days before first day of month
+    for (let i = 0; i < daysBeforeFirstDayOfMonth; i++) {
+      days.push(null);
+    }
+
+    // Add days in month
+    for (let i = 1; i <= daysInMonth(month, year); i++) {
+      days.push(new Date(year, month, i));
+    }
+
+    // Add days after last day of month
+    const daysAfterLastDayOfMonth = 6 - lastDayOfMonth.getDay();
+    for (let i = 0; i < daysAfterLastDayOfMonth; i++) {
+      days.push(null);
+    }
+
+    return days;
+  }
+
+  // Selected date
+  let selectedDate;
+  
+// Generate days for current month
+let daysInCurrentMonth = generateDaysInMonth(currentDate.getMonth(), currentDate.getFullYear());
+
+// Handle arrow click for changing month
+function handleArrowClick(direction) {
+  currentDate = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + direction,
+    1
+  );
+  daysInCurrentMonth = generateDaysInMonth(currentDate.getMonth(), currentDate.getFullYear());
+}
+
+// Handle day click to select date
+function handleDayClick(day) {
+  selectedDate = day;
+}
+
 </script>
 
-<ClubNavBar id = "{id}"/>
+<ClubNavBar id={id} />
+
+
+
+
+
 
 <section class="animate-fade-in flex-col mt-10">
-    <div class="bg-slate-500 dark:bg-slate-800 static justify-evenly flex-col p-4 md:p-12 lg:flex-row gap-2 rounded-xl items-center lg:mx-44 animate-fade-in">
-      <h1 class="text-left mb-10 text-3xl">Club Calendar</h1>
-
-    </div>
+<div class="bg-slate-500 dark:bg-slate-800 static justify-evenly flex-col p-4 md:p-12 lg:flex-row gap-2 rounded-xl items-center lg:mx-44 animate-fade-in">
+  <h1 class="text-left mb-10 text-3xl">Club Calendar</h1>
+  
+  <!-- Arrow buttons for changing month -->
+  <div class="flex items-center">
+    <button class="text-white rounded-full bg-gray-400 hover:bg-gray-600 p-2" on:click={() => handleArrowClick(-1)}>
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+      </svg>
+    </button>
+    <div class="mx-4 text-2xl">{months[currentDate.getMonth()]} {currentDate.getFullYear()}</div>
+    <button class="text-white rounded-full bg-gray-400 hover:bg-gray-600 p-2" on:click={() => handleArrowClick(1)}>
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+      </svg>
+    </button>
+  </div>
+  
+  <!-- Calendar grid -->
+  <div class="grid grid-cols-7 gap-2 mt-4">
+    {#each weekdays as day}
+      <div class="bg-slate-400 dark:bg-slate-700 py-2 rounded-xl text-white text-center font-bold">{day}</div>
+    {/each}
+    {#each daysInCurrentMonth as day}
+      <div class="bg-white dark:bg-gray-800 py-2 rounded-xl text-gray-800 dark:text-white text-center font-bold cursor-pointer transition-colors duration-200 hover:bg-gray-200 dark:hover:bg-slate-600" class:selected={day === selectedDate} on:click={() => handleDayClick(day)}>
+        {#if day !== null}
+          <span class="text-sm">{day.getDate()}</span>
+          {#each events.filter(event => event.date.toDateString() === day.toDateString()) as event}
+            <div class="text-xs">{event.title}</div>
+          {/each}
+        {/if}
+      </div>
+    {/each}
+  </div>
+  
+</div>
 </section>
