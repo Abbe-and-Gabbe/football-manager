29f15fe49a8623c92b30678339ff643bc07e4b76
Albin Forsberg <foal21fs@student.ju.se>
2023-03-01 14:38:25 +0100

    Fixed coonnection to db

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,42 @@
 import express from "express";
+import { createPool } from "mariadb";
 
 const PORT = 8080;
 
+const pool = createPool({
+    host: "db",
+    port: 3306,
+    user: "root",
+    password: "abc123",
+    database: "Football-ManagerDB",
+});
+
+// pool on error
+pool.on("error", function (err) {
+    console.log("MariaDB connection error: " + err);
+});
+
 const app = express();
+
 
 app.get("/", (req, res) => {
     console.log("Hello World!");
     res.send("Hello World!");
+});
+
+app.get("/person/:id", async (req, res) => {
+    console.log("Get person with id: " + req.params.id);
+    try {
+        const connection = await pool.getConnection();
+        const person = await connection.query(
+            "SELECT * FROM Person WHERE id = ?",
+            [req.params.id]
+        );
+        connection.release();
+        res.send(person);
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 app.listen(PORT, () => {
---
+++ backend/src/db.js
@@ -0,0 +1,5 @@
+import { createPool } from "mariadb";
+
+// Setup maria db connection pool from docker container
+
+export default pool;
