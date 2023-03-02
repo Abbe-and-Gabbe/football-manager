0a824f3b369b59f964b488a71b6550211012bf13
Albin Forsberg <foal21fs@student.ju.se>
2023-03-02 13:50:52 +0100

    Added specs for /team/:id

--- DB.session.sql
+++ DB.session.sql
@@ -1,3 +1,5 @@
-SELECT * FROM TeamPlayer
-JOIN Club ON Club.id = TeamPlayer.TeamId
-WHERE PersonId = 1
+SELECT Person.id, Person.firstName, Person.lastName, Person.email, Person.gender, Person.phoneNumber, Person.password
+FROM TeamPlayer
+JOIN Team ON TeamPlayer.TeamId = Team.id
+JOIN Person ON TeamPlayer.PersonId = Person.id
+WHERE Team.id = 1;
