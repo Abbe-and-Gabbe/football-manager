2c9f70c3c5e4ade0ac82bdbc0424702cbb3cd776
Albin Forsberg <foal21fs@student.ju.se>
2023-03-13 22:25:10 +0100

    Added club/id route

--- backend/src/routes/club.js
+++ backend/src/routes/club.js
@@ -15,6 +15,23 @@ router.get("/", async (req, res) => {
     }
 });
 
+// Returns a club with the given id
+
+router.get("/:id", async (req, res) => {
+    let data = {}
+    try {
+        const conn = await pool.getConnection();
+        const club = await conn.query("SELECT * FROM Club WHERE id = ?", [req.params.id]);
+        data.club = club[0];
+        const teams = await conn.query("SELECT * FROM Team WHERE clubId = ?", [req.params.id]);
+        data.teams = teams;
+        res.json(data);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+
 
 
 export default router;
