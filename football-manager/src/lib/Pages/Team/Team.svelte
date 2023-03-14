0fe24e73bcde73f1f5f1dc14d63c961b39b855fe
Albin Forsberg <foal21fs@student.ju.se>
2023-03-14 17:36:29 +0100

    Added api call for news

--- football-manager/src/lib/Pages/Team/Team.svelte
+++ football-manager/src/lib/Pages/Team/Team.svelte
@@ -1,6 +1,8 @@
 <script>
     import TeamNavbar from "./TeamNavbar.svelte";
+    import TeamNews from "./TeamNews.svelte";
     export let id
 </script>
 
 <TeamNavbar id={id}/>
+<TeamNews id={id} />
---
+++ football-manager/src/lib/Pages/Team/TeamNews.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let id
+    let news
+
+    const f = fetch(`http://localhost:8080/team/${id}/news`)
+
+    f.then((res) => {
+        return res.json()
+    }).then((data) => {
+        news = data
+    })
+
+</script>
+
+{id}
