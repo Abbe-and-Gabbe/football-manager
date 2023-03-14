9ad2c460140c55a7917a71420612a26eefadbef3
Albin Forsberg <foal21fs@student.ju.se>
2023-03-14 15:14:32 +0100

    Added 500ms delay, to not crach db

--- football-manager/src/lib/Header/SearchBar.svelte
+++ football-manager/src/lib/Header/SearchBar.svelte
@@ -17,6 +17,20 @@
     searchResults = data
   }
 
+  function debounce(func, wait) {
+    let timeout;
+
+    return function executedFunction(...args) {
+      const later = () => {
+        clearTimeout(timeout);
+        func(...args);
+      };
+
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+    };
+  }
+
 </script>
 
 <div
@@ -35,7 +49,10 @@
         class="block lg:w-1/2 w-full m-auto p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="Search for teams, players, etc."
         required
-        on:input={search}
+        on:input={
+          // Wait 500ms before searching
+          debounce(search, 500)
+        }
       />
     </div>
   </form>
