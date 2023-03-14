eceb2752129511f0c3f74cb1a31414bfbda89a5b
Albin Forsberg <foal21fs@student.ju.se>
2023-03-14 17:31:43 +0100

    Fixed links on navbar

--- football-manager/src/lib/Pages/Team/Team.svelte
+++ football-manager/src/lib/Pages/Team/Team.svelte
@@ -3,6 +3,4 @@
     export let id
 </script>
 
-<h1>Team</h1>
-
 <TeamNavbar id={id}/>
--- football-manager/src/lib/Pages/Team/TeamNavbar.svelte
+++ football-manager/src/lib/Pages/Team/TeamNavbar.svelte
@@ -5,10 +5,10 @@
 </script>
 
 <div class="bg-slate-400 dark:bg-slate-800 w-full rounded-xl p-2 flex gap-2">
-  <NavButton buttonText="Home" location="/club/{id}" {id} />
-  <NavButton buttonText="Teams" location="/club/{id}/teams" {id} />
-  <NavButton buttonText="Calendar" location="/club/{id}/calendar" {id} />
-  <NavButton buttonText="Contact" location="/club/{id}/contact" {id} />
+  <NavButton buttonText="Home" location="/team/{id}" {id} />
+  <NavButton buttonText="Squad" location="/team/{id}/squad" {id} />
+  <NavButton buttonText="Calendar" location="/team/{id}/calendar" {id} />
+  <NavButton buttonText="Contact" location="/team/{id}/contact" {id} />
   <!-- Only visible to admins: -->
   <NavButton buttonText="Admin" location="/team/{id}/admin" {id} />
 </div>
