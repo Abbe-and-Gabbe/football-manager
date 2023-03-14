294e4e031692aaf93ff6767738e6715b5c8a741b
Gabriel Adward <adga21bv@student.ju.se>
2023-03-14 19:03:00 +0100

    API Call

--- football-manager/src/lib/Pages/Club/Club.svelte
+++ football-manager/src/lib/Pages/Club/Club.svelte
@@ -1,24 +1,34 @@
 <script>
     export let id;
-    import teams from "../../../assets/data";
     import WelcomePage from "./WelcomePage.svelte";
     import ClubNavBar from "./clubNavBar.svelte";
 
     import ClubCardContent from "./clubCardContent.svelte";
     let club
+    let news
 
     let f = fetch(`http://localhost:8080/club/${id}`)
     f.then((res) => {
         return res.json()
     }).then((data) => {
         club = data.club
-        console.log(club)
+    })
+
+    let n = fetch(`http://localhost:8080/club/${id}/games`)
+    n.then((res) => {
+        return res.json()
+    }).then((data) => {
+        news = data.news
+        console.log(news)
+
     })
 
 </script>
 
 <ClubNavBar {id} />
 {#if club}
-<WelcomePage club={club} />
+    <WelcomePage club={club} />
 {/if}
-<ClubCardContent />
+{#if news}
+    <ClubCardContent news={news} />
+{/if}
--- football-manager/src/lib/Pages/Club/clubCardContent.svelte
+++ football-manager/src/lib/Pages/Club/clubCardContent.svelte
@@ -1,14 +1,8 @@
 <script>
-    import newsCards from "../../../assets/newsData";
 
-    let currentPage = 0;
-    function prevPage() {
-    currentPage--;
-  }
+ 
+  export let news
 
-  function nextPage() {
-    currentPage++;
-  }
 
 </script>
 
@@ -18,17 +12,12 @@
     >
     <div class="container mx-auto p-4">
         <div class="flex items-center justify-center">
-        <div class="p-4 bg-black rounded-lg shadow-md" 
-            style="display: {currentPage >= 0 && currentPage < newsCards.length ? 'block' : 'none'}">
-            <h2>{newsCards[currentPage].title}</h2>
-            <p>{newsCards[currentPage].content}</p>
+        <div class="p-4 bg-black rounded-lg shadow-md" >
+            <h2>{news.homeTeam}</h2>
+            <p>{news.awayTeam}</p>
         </div>
         </div>
-        <div class="mt-4 flex justify-between">
-            <button on:click={prevPage} class="px-4 py-2 bg-black text-white rounded-full hover:bg-gray-900" disabled={currentPage === 0}>Previous</button>
-            <span class="px-4 py-2 bg-black text-white rounded-full mr-5">{currentPage + 1} / {newsCards.length}</span>
-            <button on:click={nextPage} class="px-4 py-2 bg-black text-white rounded-full hover:bg-gray-900" disabled={currentPage === newsCards.length - 1}>Next</button>
-        </div>
+      
     </div>
     </div>
 </section>
