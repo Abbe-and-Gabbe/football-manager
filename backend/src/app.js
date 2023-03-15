62d12c2c846be1a01596ca70b1a77f3223fa2aa9
Gabriel Adward <adga21bv@student.ju.se>
2023-03-15 12:52:26 +0100

    Created a app.post for login

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,15 @@ import cors from "cors";
 import personRouter from "./routes/person.js";
 import teamRouter from "./routes/team.js";
 import clubRouter from "./routes/club.js";
+import { request } from "https";
 
 const PORT = 8080;
 
 const app = express();
 
 app.use(cors());
+
+app.use(express.urlencoded())
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -20,6 +23,12 @@ app.use("/person", personRouter);
 app.use("/team", teamRouter)
 app.use("/club", clubRouter)
 
+//login
+app.post('/tokens', function(request, response){
+    const grantType = request.body.grant_type
+    const username = request.body.username
+    const password = request.body.password
+})
 
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
