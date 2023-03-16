85c1c3e7fc6983d4fd652934e753d4494adf6e5e
Gabriel Adward <adga21bv@student.ju.se>
2023-03-16 18:55:29 +0100

    Added form to create news

---
+++ football-manager/src/lib/Pages/Club/create-news.svelte
@@ -0,0 +1,18 @@
+<div class="h-screen flex items-center justify-center bg-gray-900">
+    <form class="w-96 h-128 bg-gray-700 border-2 border-black rounded-lg p-8">
+      <div class="text-center">
+        <h3 class="text-white">Create News</h3>
+      </div>
+      <br>
+      <div class="mb-4">
+        <input class="w-full bg-gray-800 border border-gray-600 rounded-lg py-2 px-4 text-black" type="text" name="title" placeholder="Enter Your Title Here!">
+      </div>
+      <div class="mb-4">
+        <input class="w-full bg-gray-800 border border-gray-600 rounded-lg py-2 px-4 text-black" type="text" name="message" placeholder="Enter Your Message Here!">
+      </div>
+      <div class="mb-4">
+        <button class="w-full bg-blue-800 text-white py-2 px-4 rounded-lg uppercase" type="submit">Create News</button>
+      </div>
+    </form>
+  </div>
+  
