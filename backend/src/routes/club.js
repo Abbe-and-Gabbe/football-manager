b09f97b7550cd330564e9bfdcd9a0868566f7297
Gabriel Adward <adga21bv@student.ju.se>
2023-03-20 22:59:42 +0100

    I maybe add pagination to news

--- backend/src/routes/club.js
+++ backend/src/routes/club.js
@@ -60,17 +60,13 @@ router.get("/:id/teams", async (req, res) => {
 router.get("/:id/news", async (req, res) => {
     try {
         const conn = await pool.getConnection();
-        const page = req.query.page || 1; // default to page 1
-        const pageSize = req.query.pageSize || 10; // default page size to 10
-        const offset = (page - 1) * pageSize;
         const news = await conn.query(`
             SELECT News.id,title, content, published, personId, Person.firstName, Person.lastName, Team.teamName FROM Team
             JOIN News ON Team.id = News.teamId
             JOIN Person ON Person.id = News.personId
             WHERE Team.clubId = ?
             ORDER BY published DESC
-            LIMIT ? OFFSET ?
-        `, [req.params.id, pageSize, offset]);
+        `, [req.params.id]);
         console.log(news)
         res.json(news);
     } catch (err) {
@@ -78,7 +74,6 @@ router.get("/:id/news", async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 });
-
 
 // Get the upcoming matches for all the teams in a club
 
