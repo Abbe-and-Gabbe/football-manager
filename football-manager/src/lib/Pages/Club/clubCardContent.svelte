473537c0e5c66a53124dbb35f9e44f56c71bb21f
Gabriel Adward <adga21bv@student.ju.se>
2023-03-20 08:28:14 +0100

    Deleted button for news

--- football-manager/src/lib/Pages/Club/clubCardContent.svelte
+++ football-manager/src/lib/Pages/Club/clubCardContent.svelte
@@ -1,5 +1,4 @@
 <script>
-	import { Link } from 'svelte-routing'
 
   let news;
   export let id
@@ -14,11 +13,7 @@
     })
 </script>
 
-<div class="flex justify-center mt-10">
-  <a href="/club/{id}/CreateNews" class="bg-blue-500 w-3/5 text-center	 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-    Create News
-  </a>
-</div>
+
 
 
 <section class="animate-fade-in flex-col mt-10">
@@ -31,9 +26,9 @@
     <div
       class="prose dark:prose-invert p-12 border border-slate-600 rounded-xl bg-slate-300 dark:bg-slate-800 flex-row w-full text-center lg:text-left"
     >
-    <Link to="/club/{id}/news/{item.id}">
+    <h2>
       {item.title}
-    </Link>
+    </h2>
     <p class="prose dark:prose-invert truncate">
       {item.content}
     </p>
