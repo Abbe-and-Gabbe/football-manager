01f2a62109020cb332e0190713e94e8d31d70dc7
Gabriel Adward <adga21bv@student.ju.se>
2023-03-20 22:32:08 +0100

    Added Toggle

--- football-manager/src/lib/Pages/Club/DisplayTeams/DisplayAllTeams.svelte
+++ football-manager/src/lib/Pages/Club/DisplayTeams/DisplayAllTeams.svelte
@@ -1,5 +1,10 @@
 <script>
   export let teams;
+  let showTeams = false;
+
+  function toggleTeams() {
+    showTeams = !showTeams;
+  }
 </script>
 
 <section class="animate-fade-in flex-col mt-10">
@@ -7,17 +12,17 @@
     <h1 class="text-left mb-10 text-3xl">Teams</h1>
       
       <div class="w-full bg-zinc-800 dark:bg-zinc-800 p-4">
-        <h4 class="font-bold rounded-2xl border-8 border-black p-4 bg-slate-600 flex justify-center cursor-pointer">
+        <h4 class="font-bold rounded-2xl border-8 border-black p-4 bg-slate-600 flex justify-center cursor-pointer" on:click={toggleTeams}>
           Teams
         </h4> 
-        {#if Array.isArray(teams)}
+        {#if showTeams && Array.isArray(teams)}
           {#each teams as team}
-            <p>{team.teamName}</p>
+            <div class="bg-black dark:bg-slate-800 static justify-evenly flex-col p-12 lg:flex-row gap-2 rounded-xl items-center lg:mx-44 animate-fade-in">
+              <p>{team.teamName}</p>
+            </div>
           {/each}
-        {:else}
-          <p>No teams found</p>
+
         {/if}
       </div>
-
   </div>
 </section>
