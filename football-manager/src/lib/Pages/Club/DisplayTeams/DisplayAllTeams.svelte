a8767724ca9aba6bef366ceeaa338812e6e289cb
Gabriel Adward <adga21bv@student.ju.se>
2023-03-20 22:38:24 +0100

    Fixed design

--- football-manager/src/lib/Pages/Club/DisplayTeams/DisplayAllTeams.svelte
+++ football-manager/src/lib/Pages/Club/DisplayTeams/DisplayAllTeams.svelte
@@ -11,18 +11,21 @@
   <div class="bg-black dark:bg-slate-800 static justify-evenly flex-col p-12 lg:flex-row gap-2 rounded-xl items-center lg:mx-44 animate-fade-in">
     <h1 class="text-left mb-10 text-3xl">Teams</h1>
       
-      <div class="w-full bg-zinc-800 dark:bg-zinc-800 p-4">
-        <h4 class="font-bold rounded-2xl border-8 border-black p-4 bg-slate-600 flex justify-center cursor-pointer" on:click={toggleTeams}>
-          Teams
-        </h4> 
-        {#if showTeams && Array.isArray(teams)}
-          {#each teams as team}
-            <div class="bg-black dark:bg-slate-800 static justify-evenly flex-col p-12 lg:flex-row gap-2 rounded-xl items-center lg:mx-44 animate-fade-in">
-              <p>{team.teamName}</p>
-            </div>
-          {/each}
+    <div class="w-full bg-zinc-800 dark:bg-zinc-800 p-4">
+      <h4 class="font-bold rounded-2xl border-8 border-black p-4 bg-slate-600 flex justify-center cursor-pointer" on:click={toggleTeams}>
+        Teams
+      </h4> 
 
-        {/if}
-      </div>
+      {#if showTeams && Array.isArray(teams)}
+        {#each teams as team}
+          <div class="flex flex-col ml-10 mr-10">
+            <p class="border-2 border-black p-2 rounded-xl bg-slate-800">{team.teamName}</p>
+          </div>
+        {/each}
+      {:else}
+
+      {/if}
+      
+    </div>
   </div>
 </section>
