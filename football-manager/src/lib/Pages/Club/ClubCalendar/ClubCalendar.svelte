5539ee1b843d5daf9255b90e44044dc5f80c5c2c
Gabriel Adward <adga21bv@student.ju.se>
2023-03-23 13:33:09 +0100

    Made few changes

--- football-manager/src/lib/Pages/Club/ClubCalendar/ClubCalendar.svelte
+++ football-manager/src/lib/Pages/Club/ClubCalendar/ClubCalendar.svelte
@@ -1,19 +1,17 @@
 <script>
     export let id
-    let games = [];
+    let games
     import DisplayCalendar from "../../../Components/DisplayCalendar.svelte";
     import ClubNavBar from "../clubNavBar.svelte";
 
     const fetchGames = fetch(`http://localhost:8080/club/${id}/games`)
-    .then((res) => {
+    fetchGames.then((res) => {  
       return res.json();
     })
     .then((data) => {
       console.log(data);
-      if (Array.isArray(data.games)) {
-        games = data.games;
+      games = data
         console.log(games)
-      }
     })
     .catch((error) => {
       console.error(error);
