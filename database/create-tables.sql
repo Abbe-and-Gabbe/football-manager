fd9fc135c7e2dc015f2800005e3e011d42055d16
Gabriel Adward <adga21bv@student.ju.se>
2023-06-12 00:08:02 +0200

    Changed so everything is written in camel case

--- database/create-tables.sql
+++ database/create-tables.sql
@@ -1,106 +1,97 @@
-CREATE TABLE Person (
-  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  firstName varchar(255),
-  lastName varchar(255),
-  email varchar(255),
-  gender varchar(255),
-  phoneNumber varchar(255),
-  firebaseId varchar(255),
-  password varchar(255)
+CREATE TABLE person (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  firstName VARCHAR(50),
+  lastName VARCHAR(50),
+  email VARCHAR(254),
+  gender VARCHAR(255),
+  phoneNumber VARCHAR(15),
+  firebaseId VARCHAR(255)
+  /*Do we use it?*/
+,
+  password VARCHAR(255)
 );
-CREATE TABLE Club (
-  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  clubName varchar(255)
+CREATE TABLE club (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  clubName VARCHAR(255)
 );
-CREATE TABLE Team (
-  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  clubId int,
-  teamName varchar(255)
+CREATE TABLE team (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  clubId INT,
+  teamName VARCHAR(255),
+  FOREIGN KEY (clubId) REFERENCES club (id)
 );
-CREATE TABLE Training (
-  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  TeamId int,
-  startDate datetime,
-  stopDate datetime
+CREATE TABLE training (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  teamId INT,
+  startDate DATETIME,
+  stopDate DATETIME,
+  FOREIGN KEY (teamId) REFERENCES team (id)
 );
-CREATE TABLE TeamStaff (
-  PersonId int,
-  TeamId int,
-  role varchar(255)
+CREATE TABLE teamStaff (
+  personId INT,
+  teamId INT,
+  role VARCHAR(255),
+  FOREIGN KEY (personId) REFERENCES person (id),
+  FOREIGN KEY (teamId) REFERENCES team (id)
 );
-CREATE TABLE TeamPlayer (PersonId int, TeamId int);
-CREATE TABLE PlayerTraining (PersonId int, TrainingId int);
-CREATE TABLE Game (
-  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  LeagueId int,
-  homeTeam int,
-  awayTeam int,
-  homeScore int,
-  awayScore int,
-  date datetime
+CREATE TABLE teamPlayer (
+  personId INT,
+  teamId INT,
+  FOREIGN KEY (personId) REFERENCES person (id),
+  FOREIGN KEY (teamId) REFERENCES team (id)
 );
-CREATE TABLE League (
-  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  leagueName varchar(255),
-  federation varchar(255)
+CREATE TABLE playerTraining (
+  personId INT,
+  trainingId INT,
+  FOREIGN KEY (personId) REFERENCES person (id),
+  FOREIGN KEY (trainingId) REFERENCES training (id)
 );
-CREATE TABLE PlayerGame (
-  PersonId int,
-  GameId int,
-  goals int,
-  assist int,
-  yellowCard int,
-  redCard int
+CREATE TABLE game (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  leagueId INT,
+  homeTeamId INT,
+  awayTeamId INT,
+  homeScoreId INT,
+  awayScoreId INT,
+  date DATETIME,
+  FOREIGN KEY (leagueId) REFERENCES league (id),
+  FOREIGN KEY (homeTeamId) REFERENCES team (id),
+  FOREIGN KEY (awayTeamId) REFERENCES team (id)
 );
-CREATE TABLE News (
-  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  PersonId int,
-  TeamId int,
-  title varchar(255),
-  content varchar(255),
-  published datetime
+CREATE TABLE league (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  leagueName VARCHAR(255),
+  federation VARCHAR(255)
 );
-CREATE TABLE GameSummary (
-  GameId int,
-  PersonId int,
-  content varchar(255)
+CREATE TABLE playerGame (
+  personId INT,
+  gameId INT,
+  goals INT,
+  assist INT,
+  yellowCard INT,
+  redCard INT,
+  FOREIGN KEY (personId) REFERENCES person (id),
+  FOREIGN KEY (gameId) REFERENCES game (id)
 );
-ALTER TABLE Team
-ADD FOREIGN KEY (clubId) REFERENCES Club (id);
-ALTER TABLE TeamStaff
-ADD FOREIGN KEY (PersonId) REFERENCES Person (id);
-ALTER TABLE TeamStaff
-ADD FOREIGN KEY (TeamId) REFERENCES Team (id);
-ALTER TABLE TeamPlayer
-ADD FOREIGN KEY (PersonId) REFERENCES Person (id);
-ALTER TABLE TeamPlayer
-ADD FOREIGN KEY (TeamId) REFERENCES Team (id);
-ALTER TABLE Training
-ADD FOREIGN KEY (TeamId) REFERENCES Team (id);
-ALTER TABLE PlayerTraining
-ADD FOREIGN KEY (PersonId) REFERENCES Person (id);
-ALTER TABLE PlayerTraining
-ADD FOREIGN KEY (TrainingId) REFERENCES Training (id);
-ALTER TABLE Game
-ADD FOREIGN KEY (LeagueId) REFERENCES League (id);
-ALTER TABLE Game
-ADD FOREIGN KEY (homeTeam) REFERENCES Team (id);
-ALTER TABLE Game
-ADD FOREIGN KEY (awayTeam) REFERENCES Team (id);
-ALTER TABLE PlayerGame
-ADD FOREIGN KEY (PersonId) REFERENCES Person (id);
-ALTER TABLE PlayerGame
-ADD FOREIGN KEY (GameId) REFERENCES Game (id);
-ALTER TABLE News
-ADD FOREIGN KEY (PersonId) REFERENCES Person (id);
-ALTER TABLE News
-ADD FOREIGN KEY (TeamId) REFERENCES Team (id);
-ALTER TABLE GameSummary
-ADD FOREIGN KEY (GameId) REFERENCES Game (id);
-ALTER TABLE GameSummary
-ADD FOREIGN KEY (PersonId) REFERENCES Person (id);
+CREATE TABLE news (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  personId INT,
+  teamId INT,
+  title VARCHAR(255),
+  content VARCHAR(255),
+  published DATETIME,
+  FOREIGN KEY (personId) REFERENCES person (id),
+  FOREIGN KEY (teamId) REFERENCES team (id)
+);
+CREATE TABLE gameSummary (
+  gameId INT,
+  personId INT,
+  content VARCHAR(255),
+  FOREIGN KEY (gameId) REFERENCES game (id),
+  FOREIGN KEY (personId) REFERENCES person (id)
+);
 -- Insert test data into Person table
-INSERT INTO Person (
+INSERT INTO person (
     firstName,
     lastName,
     email,
@@ -148,20 +139,20 @@ VALUES (
     '555-1357',
     'password'
   );
--- Insert test data into Club table
-INSERT INTO Club (clubName)
+-- Insert test data into club table
+INSERT INTO club (clubName)
 VALUES ('Manchester United'),
   ('Chelsea'),
   ('Arsenal');
--- Insert test data into Team table
-INSERT INTO Team (clubId, teamName)
+-- Insert test data into team table
+INSERT INTO team (clubId, teamName)
 VALUES (1, 'Manchester United A'),
   (1, 'Manchester United B'),
   (2, 'Chelsea A'),
   (2, 'Chelsea B'),
   (3, 'Arsenal A');
--- Insert test data into TeamStaff table
-INSERT INTO TeamStaff (PersonId, TeamId, role)
+-- Insert test data into teamStaff table
+INSERT INTO teamStaff (personId, teamId, role)
 VALUES (1, 1, 'Head Coach'),
   (2, 1, 'Assistant Coach'),
   (3, 2, 'Head Coach'),
@@ -172,8 +163,8 @@ VALUES (1, 1, 'Head Coach'),
   (3, 4, 'Physiotherapist'),
   (4, 4, 'Fitness Coach'),
   (5, 5, 'Head Coach');
--- Insert test data into TeamPlayer table
-INSERT INTO TeamPlayer (PersonId, TeamId)
+-- Insert test data into teamPlayer table
+INSERT INTO teamPlayer (personId, teamId)
 VALUES (1, 1),
   (2, 1),
   (3, 1),
@@ -184,15 +175,15 @@ VALUES (1, 1),
   (3, 3),
   (4, 4),
   (5, 5);
--- Insert test data into Training table
-INSERT INTO Training (TeamId, startDate, stopDate)
+-- Insert test data into training table
+INSERT INTO training (teamId, startDate, stopDate)
 VALUES (1, '2023-03-01 10:00:00', '2023-03-01 12:00:00'),
   (2, '2023-03-01 10:00:00', '2023-03-01 12:00:00'),
   (3, '2023-03-01 10:00:00', '2023-03-01 12:00:00'),
   (4, '2023-03-01 10:00:00', '2023-03-01 12:00:00'),
   (5, '2023-03-01 10:00:00', '2023-03-01 12:00:00');
--- Insert test data into PlayerTraining table
-INSERT INTO PlayerTraining (PersonId, TrainingId)
+-- Insert test data into playerTraining table
+INSERT INTO playerTraining (personId, TrainingId)
 VALUES (1, 1),
   (2, 1),
   (3, 1),
@@ -203,20 +194,20 @@ VALUES (1, 1),
   (3, 3),
   (4, 4),
   (5, 5);
--- Insert test data into League table
-INSERT INTO League (leagueName, federation)
+-- Insert test data into league table
+INSERT INTO league (leagueName, federation)
 VALUES ('Premier League', 'FA'),
   ('Championship', 'FA'),
   ('League One', 'FA'),
   ('League Two', 'FA'),
   ('National League', 'FA');
--- Insert test data into Game table
-INSERT INTO Game (
-    LeagueId,
-    homeTeam,
-    awayTeam,
-    homeScore,
-    awayScore,
+-- Insert test data into game table
+INSERT INTO game (
+    leagueId,
+    homeTeamId,
+    awayTeamId,
+    homeScoreId,
+    awayScoreId,
     date
   )
 VALUES (1, 1, 2, 2, 1, '2023-03-01 10:00:00'),
@@ -229,10 +220,10 @@ VALUES (1, 1, 2, 2, 1, '2023-03-01 10:00:00'),
   (1, 4, 3, 1, 2, '2023-03-04 14:00:00'),
   (1, 5, 1, 2, 1, '2023-03-01 16:00:00'),
   (1, 5, 1, 2, 1, '2023-03-03 16:00:00');
--- Insert test data into PlayerGame table
-INSERT INTO PlayerGame (
-    PersonId,
-    GameId,
+-- Insert test data into playerGame table
+INSERT INTO playerGame (
+    personId,
+    gameId,
     goals,
     assist,
     yellowCard,
@@ -263,8 +254,8 @@ VALUES (1, 1, 1, 0, 0, 0),
   (3, 9, 0, 0, 0, 1),
   (4, 10, 0, 0, 0, 1),
   (5, 10, 0, 0, 0, 1);
--- Insert test data into GameSummary table
-INSERT INTO GameSummary (GameId, PersonId, content)
+-- Insert test data into gameSummary table
+INSERT INTO gameSummary (gameId, personId, content)
 VALUES (1, 1, 'Game 1 summary'),
   (2, 1, 'Game 2 summary'),
   (3, 1, 'Game 3 summary'),
@@ -275,8 +266,8 @@ VALUES (1, 1, 'Game 1 summary'),
   (8, 1, 'Game 8 summary'),
   (9, 1, 'Game 9 summary'),
   (10, 1, 'Game 10 summary');
--- Insert test data into News table
-INSERT INTO News (PersonId, TeamId, title, content, published)
+-- Insert test data into news table
+INSERT INTO news (personId, teamId, title, content, published)
 VALUES (
     1,
     1,
